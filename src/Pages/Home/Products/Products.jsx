d4041f72c9ae1ea8748a858f01ed9c5c7ef7a97e
hmdmasudsarder <hmmasudsarder@gmail.com>
2023-11-07 04:19:48 +0600

    error page create

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -3,7 +3,7 @@ const Products = () => {
         <div className="mt-7">
             <div className="text-center">
                 <p className="text-2xl text-orange-400 font-bold">Service</p>
-                <p className="text-5xl font-bold">Our Service Area</p>
+                <p className="text-4xl font-bold">TOP SIX ORDERED FOOD</p>
                 <p className="text-muted">the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
             </div>
         </div>
--- src/components/SharedComponents/Error/Error.jsx
+++ src/components/SharedComponents/Error/Error.jsx
@@ -1,23 +1,20 @@
+import { Link } from "react-router-dom";
+import imgError from '../../../assets/image/Capture3-3.png';
+
 const Error = () => {
   return (
     <>
       <div className="h-screen flex justify-center items-center">
-        <div className="grid h-screen px-4 bg-white place-content-center">
+        <div className="grid h-screen bg-white place-content-center">
           <div className="text-center">
-            <h1 className="font-black text-gray-200 text-9xl">404</h1>
-
-            <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Uh-oh!
-            </p>
-
-            <p className="mt-4 text-gray-500">We can't find that page.</p>
-
+            <img src={imgError} alt="" />
             <Link
               to="/"
-              className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+              className="inline-block px-10 py-6 mt-6 text-sm font-medium top-10 text-white bg-red-600 rounded hover:bg-orange-700 focus:outline"
             >
               Go Back Home
             </Link>
+            <span className="py-10"></span>
           </div>
         </div>
       </div>
